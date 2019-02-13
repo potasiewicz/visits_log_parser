067dde859037d9f5c6edd9c8384b26af23741798
Paweł Potasiewicz <potasiewicz@gmail.com>
2019-02-13 22:51:11 +0100

    check input file format

--- app/file_reader.rb
+++ app/file_reader.rb
@@ -3,17 +3,25 @@
 class FileReader
   def initialize(file)
     @file = file
+    @file_format_error = 'Check logs file format. File with page visits logs should looks like "#path #IP"'
   end
 
   def to_hash
     file_as_hash = {}
     File.readlines(@file).each do |line|
       split_line = line.split
+      validate_line split_line
       ip = split_line[1]
       path = split_line[0]
       hash_value = file_as_hash[path] ? file_as_hash[path] << ip : [ip]
       file_as_hash[path] = hash_value
     end
     file_as_hash
+  end
+
+  private
+
+  def validate_line(split_line)
+    raise @file_format_error if split_line.size < 2
   end
 end
--- visit_log_parser.rb
+++ visit_log_parser.rb
@@ -6,4 +6,6 @@ begin
   VisitsLogApp.new(ARGV[0]).parse
 rescue ArgumentError => e
   puts e.message
+rescue RuntimeError => e
+  puts e.message
 end
