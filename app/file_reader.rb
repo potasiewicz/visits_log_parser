fe90e11238df0839ad015c201141198b3687c8a4
Paweł Potasiewicz <potasiewicz@gmail.com>
2019-02-13 20:47:31 +0100

    refactoring - method was too long

--- app/file_reader.rb
+++ app/file_reader.rb
@@ -9,12 +9,10 @@ class FileReader
     file_as_hash = {}
     File.readlines(@file).each do |line|
       split_line = line.split
-      if file_as_hash[split_line[0]]
-        hash_value = file_as_hash[split_line[0]] << split_line[1]
-      else
-        hash_value = [split_line[1]]
-      end
-      file_as_hash[split_line[0]] = hash_value
+      ip = split_line[1]
+      path = split_line[0]
+      hash_value = file_as_hash[path] ? file_as_hash[path] << ip : [ip]
+      file_as_hash[path] = hash_value
     end
     file_as_hash
   end
